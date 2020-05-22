babf38d409def5f50d130b938b74b665f748785a
teeto11 <elteeto11@gmail.com>
2020-05-22 03:28:11 +0100

    react-redux boilerplate construction

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Market from './pages/Market'
+
+function App() {
+  return (
+    <div className="App">
+      <Market />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Market.js
@@ -0,0 +1,11 @@
+import React,{useState} from  'react'
+import{useDispatch,useSelector} from 'react-redux'
+
+function Market(){
+    return(
+        <div>
+          <p>Agro Market Place </p>
+        </div>
+    )
+}
+export default Market
